93120b7324c9e3e6a4f87de8aabf7fa1f79d72ed
yb6b <seetxx@qq.com>
2023-01-31 01:12:30 +0800

    change into input element

--- src/components/ZingenAnki.svelte
+++ src/components/ZingenAnki.svelte
@@ -1,5 +1,6 @@
 <script>
   import { onMount } from "svelte";
+  import { writable } from "svelte/store";
   import { Schedule } from "./schedule";
   export let cards;
   export let name;
@@ -7,21 +8,29 @@
   let thisSchedule;
   let item = [" ", "", ""];
   let progress = 0;
-  let userKeys = "";
   let isFirstLearn = true;
   let isCorrect = true;
   const fiveStrokes = new Set("⼀⼂⺂⼁⼃");
+  const userKeys = writable("");
+  let lastKey = "";
+
   onMount(() => {
     thisSchedule = new Schedule(cards, name);
     const first = thisSchedule.first();
     item = first.item;
     isFirstLearn = first.isFirst;
     progress = thisSchedule.progress;
+
+    userKeys.subscribe((v) => {
+      if (v === "") return;
+      checkForNextItem(v[0]);
+      userKeys.set("");
+      lastKey = v;
+    });
   });
 
-  function handleUserKeyDown(e) {
-    if (e.keyCode < 65 || e.keyCode > 90) return;
-    const answer = e.key.toLowerCase();
+  function checkForNextItem(answer) {
+    if (answer.length !== 1) return;
     let next;
     if (answer === item[1]) {
       next = thisSchedule.nextSuccess();
@@ -30,14 +39,12 @@
       next = thisSchedule.nextFail();
       isCorrect = false;
     }
-    userKeys = answer;
     item = next.item;
     isFirstLearn = next.isFirst;
     progress = thisSchedule.progress;
   }
 </script>
 
-<svelte:body on:keydown={handleUserKeyDown} />
 <div class="columns is-centered">
   <div class="column is-half">
     <p class="has-text-grey">训练进度：</p>
@@ -47,24 +54,34 @@
     <div class="notification has-text-centered ">
       <div class="columns">
         <div class="column is-8">
-          <div class="has-text-grey-light">请敲击字根所在按键</div>
-
           <div class="title is-size-1 pt-4 zigenfont has-text-info">
             {item[0]}
           </div>
+          <input
+            bind:value={$userKeys}
+            class="input is-info is-rounded has-text-centered has-text-weight-bold is-family-code"
+            style="max-width: 9rem;"
+            type="text"
+            autocomplete="none"
+            placeholder="输入字根键"
+            autofocus="autofocus"
+          />
         </div>
-        {#if fiveStrokes.has(item[0])}
-          <div class="column">
-            <div class="has-text-dark-info mb-1">该字是小码元的五个笔画键</div>
-          </div>
-        {:else}
-          <div class="column has-text-left">
+        <div class="column m-auto has-text-left">
+          {#if fiveStrokes.has(item[0])}
+            <div class="has-text-dark-info mb-1">五个笔画键</div>
+          {:else}
             <div class="has-text-dark-info mb-1">相关汉字：</div>
-            <div class="has-text-grey is-size-7">{item[2]}</div>
-          </div>
-        {/if}
+            <div class="has-text-grey is-size-7" style="letter-spacing: 1px;">
+              {item[2]}
+            </div>
+          {/if}
+        </div>
       </div>
-      <div class="has-text-grey-dark" class:is-invisible={!isFirstLearn && isCorrect}>
+      <div
+        class="has-text-grey-dark"
+        class:is-invisible={!isFirstLearn && isCorrect}
+      >
         答案是 <span class="is-family-code has-text-info has-text-weight-bold"
           >{item[1]}</span
         >
@@ -72,16 +89,11 @@
       </div>
       <div class="has-text-grey" class:is-invisible={isCorrect}>
         <span>你的回答</span>
-        <span
-          class="has-text-danger has-text-weight-bold is-family-code"
-          id="yr">{userKeys}</span
+        <span class="has-text-danger is-family-code has-text-weight-bold"
+          >{lastKey}</span
         >
         <span class="has-text-danger has-text-weight-bold">错误</span>
       </div>
     </div>
-    <!-- <div class="buttons has-addons is-centered">
-        <button class="button  is-inverted is-small">网页键盘</button>     
-        <button class="button  is-inverted is-small">字根图</button>     
-    </div> -->
   </div>
 </div>
